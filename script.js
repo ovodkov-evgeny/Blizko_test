dc7a4a686fa36747bbda018bf7876abe12ea0bb9
Evgeny Ovodkov <kefir4ek@gmail.com>
2023-05-22 20:48:30 +0300

    Test task

--- script.js
+++
@@ -1,7 +0,0 @@
-const headerBtn = document.querySelector(".header__burger-btn"),
-  menu = document.querySelector(".menu"),
-  mobileHeader = document.querySelector(".mobile__header");
-
-headerBtn.addEventListener("click", () => {
-  mobileHeader.classList.toggle("open");
-});
